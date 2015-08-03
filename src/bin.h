c3b031df4b0d40b51bb13a1d88f1f6c126015204
Eric Graves <eric.graves@lexisnexis.com>
2015-08-03 14:12:54 -0500

    adding bin.h

---
+++ src/bin.h
@@ -0,0 +1,31 @@
+#ifndef BIN_H
+#define BIN_H
+
+#include "R.h"
+#include "variable.h"
+#include "queue.h"
+#include "xtab.h"
+
+#define RETURN_R
+
+struct iv {
+  double asc_woe;
+  double dsc_woe;
+  double iv;
+};
+
+struct opts {
+  double min_iv;
+  int min_cnt;
+  int max_bin;
+  int mono;
+};
+
+// prototype for main working function
+size_t find_best_split(int start, int stop, struct xtab* xtab, double* grand_tot, struct opts opts);
+
+SEXP bin(SEXP x, SEXP y, SEXP miniv, SEXP mincnt, SEXP maxbin, SEXP monotonicity);
+
+struct iv calc_iv(double* asc_cnts, double* dsc_cnts, double* tots);
+
+#endif
