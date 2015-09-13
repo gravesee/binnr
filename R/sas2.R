b32e4cd34f271b119e7268cdf721a998c56b4e57
Zelazny7 <gravcon5@gmail.com>
2015-09-12 22:40:40 -0500

    working on SAS code for rcs and dists

--- R/sas2.R
+++ R/sas2.R
@@ -1,14 +1,19 @@
 
-# pieces common to both factor and numeric translation
-base.sas <- function(x) {
-  
-  
-  
-  
+sas <- function(x, type, sfx, coef) {
+  UseMethod("sas", x)
 }
 
+# pieces common to both factor and numeric translation
 #' @export
-sas.bin.numeric <- function(b, p, sfx) {
+sas.bin.numeric <- function(b, sfx='w', type="woe", coef=1) {
+  if (type == "woe") {
+    p <- pred.list.woe(b)
+  } else if (type == "rcs") {
+    p <- pred.list.rcs(b)
+  } else if (type == "dist") {
+    p <- pred.list.dist(b, coef)
+  }
+  
   v <- b$name
   vs <- paste(b$name, sfx, sep='_')
   
@@ -39,7 +44,6 @@ sas.bin.numeric <- function(b, p, sfx) {
 
 # interface to the SAS function:
 pred.list.woe <- function(b) {
-  
   # var woe
   var <- b$core$values$var
   names(var) <- b$core$breaks[-1]
@@ -58,22 +62,64 @@ pred.list.dist <- function(b, coef=1) {
   names(var) <- b$core$breaks[-1]
   
   list(
-    nas=0,
+    nas=min(var) - 0,
     exc=b$core$values$exc,
     var=var)
 }
 
 pred.list.rcs <- function(b, coef=1) {
   if (is.null(b$rcs)) {
-    rcs(b) <- "''"
+    rcs(b) <- ""
   }
   
   # var woe
   var <- dQuote(b$rcs$var)
   names(var) <- b$core$breaks[-1]
   
+  nas <- if(length(b$rcs$nas) == 0) "" else b$rcs$nas
+  
   list(
-    nas=dQuote(b$rcs$nas),
+    nas=dQuote(nas),
     exc=dQuote(b$rcs$exc),
     var=var)
+}
+
+
+# pieces common to both factor and numeric translation
+#' @export
+sas.bin.factor <- function(b, sfx='w', type="woe", coef=1) {
+  if (type == "woe") {
+    p <- pred.list.woe(b)
+  } else if (type == "rcs") {
+    p <- pred.list.rcs(b)
+  } else if (type == "dist") {
+    p <- pred.list.dist(b, coef)
+  }
+  
+  v <- b$name
+  vs <- paste(b$name, sfx, sep='_')
+  
+  text <- list(sprintf("  * Variable: %s ;\n", v))
+  
+  # missing text
+  txt1 <- sprintf("    if missing(%s)", v)
+  txt2 <- sprintf("\n        then %s = %s;\n", vs, p$nas)
+  text[[length(text)+1]] <- sprintf("%-50s%s" , txt1, txt2)
+  
+  # exceptions text
+  for (i in seq_along(p$exc)) {
+    txt1 <- sprintf("    else if %s = %s", v, names(p$exc)[i])
+    txt2 <- sprintf("\n        then %s = %s;\n", vs, p$exc[i])
+    text[[length(text)+1]] <- sprintf("%-50s%s" , txt1, txt2)
+  }
+  
+  for (i in ) {
+    txt1 <- sprintf("    else if %s <= %s", v, names(p$var)[i])
+    txt2 <-
+      sprintf("\n        then %s = %s;\n", vs, p$var[i])
+    text[[length(text)+1]] <- sprintf("%-50s%s" , txt1, txt2)
+  }
+  text[[length(text)+1]] <-
+    sprintf("    else %s = %s;\n", vs, p$var[i+1])
+  do.call(c, text)
 }
