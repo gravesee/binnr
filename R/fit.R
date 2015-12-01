fa7f7a0c2682d83c269355adb5edcc05ef04a8e5
Eric Graves <eric.graves@lexisnexis.com>
2015-12-01 06:48:52 -0600

    added docs for fit and trim

--- R/fit.R
+++ R/fit.R
@@ -1,17 +1,49 @@
+#' fit binnr objects
+#' 
+#' \code{fit} is the main engine for fitting binnr objects to data.
+#' 
+#' @param x
+#' 
+#' @details \code{fit} does a lot of heavy lifting for the user. It will apply
+#' the weight of evidence transformations stored in the bins of the passed object.
+#' If provided a dataset, it will apply these transformations on the dataset. If
+#' not given a dataset, \code{fit} will use the data that was provided to create
+#' the bins.
+#' 
+#' The transformed data is passed to \code{cv.glmnet} to fit a LASSO regression
+#' model.
+#' 
+#' @param x a \code{bin.list}, \code{binnr.model}, or \code{segmented} object
+#' @param data a \code{data.frame} with a names attribute or NULL
+#' @param y a numeric vector the same length as the number of rows in \code{data}
+#' or NULL
+#' @param nfolds Number of cross-validation folds for \code{cv.glmnet}
+#' @param lower.limits minimum value of coefficients in the glmnet model
+#' @param upper.limits maximum value of coefficients in the glmnet model
+#' @param family Link function family to pass on to \code{cv.glmnet}
+#' @param alpha parameter passed to \code{cv.glmnet}. 0 == LASSO, 1 == Ridge
+#' @param drop boolean indicator for whether to flag variables not in the model
+#' as "dropped" which will prevent them from being fit in subsequent iterations.
+#' 
+#' @return a \code{binnr.model} or \code{segmented} object depending on
+#' what was passed.
 
 #' @export
-fit <- function(x, ...) {
+fit <- function(x, data=NULL, y=NULL, nfolds=3, lower.limits=0, upper.limits=3,
+                family="binomial", alpha=1, drop=F) {
   UseMethod("fit")
 }
 
 #' @export
-fit.binnr.model <- function(mod, data, y, nfolds=3, lower.limits=0, upper.limits=3, family="binomial", alpha=1, drop=F) {
+fit.binnr.model <- function(mod, data=NULL, y=NULL, nfolds=3, lower.limits=0,
+                            upper.limits=3, family="binomial", alpha=1, drop=F) {
   argg <- as.list(environment())
   do.call(fit.bin.list, c(list(bins=mod$bins), argg[-1]))
 }
 
 #' @export
-fit.bin.list <- function(bins, data=NULL, y=NULL, nfolds=3, lower.limits=0, upper.limits=3, family="binomial", alpha=1, drop=F) {
+fit.bin.list <- function(bins, data=NULL, y=NULL, nfolds=3, lower.limits=0,
+                         upper.limits=3, family="binomial", alpha=1, drop=F) {
   if (is.null(y)) y <- bins[[1]]$data$y # grab the first y from a bin
   x <- predict(bins, data)
   
@@ -29,23 +61,29 @@ fit.bin.list <- function(bins, data=NULL, y=NULL, nfolds=3, lower.limits=0, uppe
   
   if (drop) drop(bins) <- seq_along(bins)[-k]
   
-  mod <- binnr.model(bins, betas)
-  mod$contribution <- sort(predict(mod, data, y, type="contribution"), decreasing = T)
+  bins.reorder <- bin.list(c(bins[k], bins[-k])) # put the mod vars first
+  mod <- binnr.model(bins.reorder, betas)
+  mod$contribution <- sort(predict(mod, data, y, type="contribution"),
+                           decreasing = T)
   mod
 }
 
 #' @export
-fit.segmented <- function(obj, data=NULL, y=NULL, seg, nfolds=3, lower.limits=0, upper.limits=3, family="binomial", alpha=1, drop=F) {
+fit.segmented <- function(obj, data=NULL, y=NULL, seg, nfolds=3, lower.limits=0,
+                          upper.limits=3, family="binomial", alpha=1, drop=F) {
   argg <- as.list(environment())[-(1:5)]
   
   # use data stored with objects if none is passed in
   if (is.null(data) & is.null(y)) {
-    structure(lapply(obj, function(x) {do.call(fit, c(list(x), argg))}),
-              class = c("segmented"))
+    structure(
+      lapply(obj, function(x) {do.call(fit, c(list(x), argg))}),
+    class = c("segmented"))
   } else {
     # else use the passed in data
     xs <- split(data, seg, drop=T)
     ys <- split(y, seg, drop=T)
-    structure(mapply(fit, obj, xs, ys, MoreArgs = argg, SIMPLIFY = F), class = c("segmented"))
+    structure(
+      mapply(fit, obj, xs, ys, MoreArgs = argg, SIMPLIFY = F),
+    class = c("segmented"))
   }
 }
--- R/trim.R
+++ R/trim.R
@@ -1,4 +1,15 @@
-# functions for trimming variables from binnr.models
+#' Trim
+#' 
+#' Trim variables from binnr.models based on contribution
+#' 
+#' @param x A \code{binnr.model} or \code{segmented} object
+#' @param contribution the contribution threshold. Variables below this
+#' threshold will be flagged as dropped for subsequent \code{fit}
+#' operations.
+#' 
+#' @return an object of the same type that was provided in \code{x}. Variables
+#' below the passed contribution threshold are now flagged as dropped.
+#' 
 #' @export
 trim <- function(x, contribution=0) {
   UseMethod("trim", x)
